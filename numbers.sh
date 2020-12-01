a08e9c18dabc8d8fead5fd6f718229af1c343c30
Santiago Crisantes <crisantes@chapman.edu>
2020-11-30 22:13:42 -0700

    add numbers.sh

---
+++ numbers.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#number.sh
+#Santiago Crisantes
+
+echo "Enter a positive number"
+read -r  number
+
+N=1
+while [ "$N" -le "$number" ]
+do
+
+	if $((N%2)) -eq 0
+	then
+		echo "$N is even"
+	else
+		echo "$N is odd"
+	fi
+
+	N=$ [ $N+1 ]
+done
